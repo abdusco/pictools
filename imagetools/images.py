b71fa1517401e2da265150762e08cf29c6ea8369
Abdussamet Kocak <sssmmt@gmail.com>
2019-04-12 21:25:29 +0300

    Improve logging

--- imagetools/images.py
+++ imagetools/images.py
@@ -18,12 +18,9 @@ def process_dir(source_dir: Path, target_dir: Path, max_length: int, quality: in
 
     processed = []
     total = len(images)
-    for i, img in enumerate(images):
-        logger.debug(f'Processing {i:3d}/{total}: {img}')
-
+    for i, img in enumerate(images, start=1):
         save_path: Path = target_dir / f'{img.stem}_max{max_length}q{quality}{img.suffix}'
         if not force and save_path.exists():
-            logger.debug(f'Done: {img}')
             continue
 
         with Image.open(img) as image:
@@ -40,6 +37,7 @@ def process_dir(source_dir: Path, target_dir: Path, max_length: int, quality: in
         before_bytes, after_bytes = [f.stat().st_size for f in [img, save_path]]
         before_size, after_size = [fs.readable_size(s) for s in [before_bytes, after_bytes]]
         change_percent = (after_bytes - before_bytes) / before_bytes * 100
-        logger.debug(f'Done: {before_size} -> {after_size} ({change_percent:.0f}%)')
+        size_report = f'{before_size} -> {after_size} ({change_percent:.0f}%)'
+        logger.debug(f'Done [{i:3d}/{total:3d}]: {save_path}\n{size_report}')
 
     return processed
