f6c66932d6670ffd423d11817b976f8712b29852
Abdussamet Kocak <sssmmt@gmail.com>
2019-04-14 15:29:56 +0300

    Read requirements from file

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    required = f.readlines()
+
 setup(
     name='pictools',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'Click',
-        'Pillow',
-        'loguru'
-    ],
+    install_requires=required,
     entry_points='''
         [console_scripts]
         pictools=main:cli
